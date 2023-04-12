20e026b2bbf6064fbd3bed6ce145695d9e45797d
Rodolphe Herbin <84749107+rherbin@users.noreply.github.com>
2023-04-12 20:12:28 +0200

    Replaced all code translating 3D to 2D with numpy code
    Just learned numpy, pretty cool

--- 3dplot.py
+++ 3dplot.py
@@ -1,12 +1,12 @@
 import pygame as pg
 import sys
-from math import sqrt,sin,cos,pi,floor
+from math import sqrt,sin,cos,pi,factorial
+import numpy as np
 
 winsize = 500
 display = pg.display.set_mode((winsize,winsize))
 clock = pg.time.Clock()
 
-func = lambda x,y:sin(x**2)+cos(y**2)
 rx = [[1,0,0],
       [0,0,-1],
       [0,1,0]]
@@ -19,6 +19,11 @@ rz = [[1,0,0],
       [0,0,-1],
       [0,1,0]]
 
+def rot_mat(angle):
+    return np.array([[cos(angle),-sin(angle),0],[sin(angle),cos(angle),0],[0,0,1]])
+
+isomat = np.array([[1,-1,0],[1/2,1/2,-1]])
+
 def iso(coords):
     x = 1/sqrt(2) * (coords[0] - coords[1])
     y = 1/sqrt(6) * (coords[0] + 2*coords[2] + coords[1])
@@ -27,25 +32,87 @@ def iso(coords):
     #z = 1/sqrt(6) * sqrt(2) * (coords[0] - coords[1] + coords[2])
     return (x+winsize/2,y+winsize/2),(x2+winsize/2,y2+winsize/2)
 
-def plot(func=func ,delta=2, divdensity=2, height=1, rotate=0, zoom=1):
-    values = [[( (x*delta*divdensity) / (100) , (y*delta*divdensity) / (100) , func((x*delta*divdensity) / (100),(y*delta*divdensity) / (100))) for x in range(-100//divdensity,100//divdensity+1)] for y in range(-100//divdensity,100//divdensity+1)]
-    print(values[0][0],values[-1][-1])
-    for x in range(len(values)):
-        for y in range(len(values[x])):
-            #cx,cy = iso(values[x][y])[0]
-            #cz = 0.0
+class Graph:
+
+    def __init__(self,func=lambda x,y:0,delta=2,divdensity=2,stpnb=False):
+        self.func = func
+        self.stpnb = stpnb
+        self.evaluate(delta,divdensity)
+    
+    def evaluate(self,delta=2,divdensity=2):
+
+        if self.stpnb:
+            values = []
+            for y in range(-100//divdensity,100//divdensity+1):
+                line = []
+                for x in range(-100//divdensity,100//divdensity+1):
+                    rx = (x*delta*divdensity)/(100)
+                    ry = (y*delta*divdensity)/(100)
+                    line.append([rx, ry, self.func(rx,ry)])
+                values.append(line)
+            #self.values = [[( (x*delta*divdensity) / (100) , (y*delta*divdensity) / (100) , func((x*delta*divdensity) / (100),(y*delta*divdensity) / (100))) for x in range(-100//divdensity,100//divdensity+1)] for y in range(-100//divdensity,100//divdensity+1)]
+            self.values = values
+
+        else:
+            values = []
+            for y in range(-(delta*100)//divdensity,(delta*100)//divdensity+1):
+                line = []
+                for x in range(-(delta*100)//divdensity,(delta*100)//divdensity+1):
+                    rx = (x*divdensity)/(100)
+                    ry = (y*divdensity)/(100)
+                    line.append([rx, ry, self.func(rx,ry)])
+                values += line
+            self.values = np.array(values)
+
+    def plot(self, height=1, rotate=0, zoom=1):
+        axis = [[[-1000,0,0],[1000,0,0]],[[0,-1000,0],[0,1000,0]],[[0,0,-1000],[0,0,1000]]]
+        rot = rot_mat(rotate)
+        for x in self.values:
             
-            fx,fy = values[x][y][0],values[x][y][1]
+            coords = x
+            coords = np.matmul(rot,coords)
+            coords[2] *= height
+            coords = np.matmul(isomat,coords)*zoom
+
+            coords += winsize/2
+
+            #cx = (fx - fy) * zoom + winsize/2
+            #cy = ((fx + fy)/2) * zoom  + winsize/2
+            #cz = self.values[x][y][2] * height * zoom
+
+            pg.draw.circle(display,"white",coords,1)
+        for x in axis:
+            fx,fy = x[0][0],x[0][1]
             cosr,sinr = cos(rotate),sin(rotate)
             fx,fy = fx*cosr-fy*sinr,fx*sinr+fy*cosr
 
-            cx = (fx - fy) * zoom + winsize/2
-            cy = ((fx + fy)/2) * zoom  + winsize/2
-            cz = values[x][y][2] * height * zoom
-            #pg.draw.line(display,"black",(cx,cy-cz),(cx,cy),1)
-            pg.draw.circle(display,"white",(cx,cy-cz),1)
+            cx = (fx - fy) + winsize/2
+            cy = ((fx + fy)/2) + winsize/2
+            cz = x[0][2]
+            cord1 = (cx,cy-cz)
+
+            fx,fy = x[1][0],x[1][1]
+            cosr,sinr = cos(rotate),sin(rotate)
+            fx,fy = fx*cosr-fy*sinr,fx*sinr+fy*cosr
+
+            cx = (fx - fy) + winsize/2
+            cy = ((fx + fy)/2) + winsize/2
+            cz = x[1][2]
+            cord2 = (cx,cy-cz)
+
+            pg.draw.line(display,"white",cord1,cord2,1)
 
 rotate = 0
+zoom = 1
+height = 1
+delta = 2
+
+try:
+    func = eval("lambda x,y:"+input("Input a function to plot (with x and y variables) : "))
+except SyntaxError:
+    func = lambda x,y:cos(x**2)+sin(y**2)
+
+graph = Graph(func=func,divdensity=8,delta=2,stpnb=False)
 
 while True:
     display.fill((0,0,0))
@@ -54,15 +121,30 @@ while True:
         if event.type == pg.QUIT:
             pg.quit()
             sys.exit()
+        if event.type == pg.MOUSEWHEEL:
+            if event.y == 1:
+                zoom*=2
+            elif event.y==-1:
+                zoom*=(1/2)
 
     keys = pg.key.get_pressed()
 
     if keys[pg.K_RIGHT]:
-        rotate += pi/16
+        rotate += pi/32
     if keys[pg.K_LEFT]:
-        rotate -= pi/16
+        rotate -= pi/32
+    if keys[pg.K_UP]:
+        height*=1.5
+    if keys[pg.K_DOWN]:
+        height*=1/1.5
+    if keys[pg.K_e]:
+        delta +=1
+        graph.evaluate(delta)
+    if keys[pg.K_r]:
+        delta -= 1
+        graph.evaluate(delta)
 
-    plot(divdensity=4,height=1,rotate=rotate,zoom=30, delta=4)
+    graph.plot(height=height,rotate=rotate,zoom=zoom)
 
-    clock.tick(10)
+    clock.tick(60)
     pg.display.update()
